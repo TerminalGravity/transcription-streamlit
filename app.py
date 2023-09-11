2975d5e6650d41d3657ee9289df74bb43ea100f5
terminalgravity <jrinnfelke@gmail.com>
2023-09-11 12:39:12 -0700

    Refactor code to remove authentication and use only Streamlit

--- app.py
+++ app.py
@@ -1,34 +1,10 @@
 import streamlit as st
 from azure_storage import AzureStorage
 from openai_transcription import OpenAITranscription
-from google_auth import google_auth, google_auth_callback
-from flask import Flask, redirect, url_for, session
-from flask import render_template
-from config import APP_SECRET_KEY
-
-app = Flask(__name__)
-app.secret_key = APP_SECRET_KEY
 
 azure_storage = AzureStorage()
 openai_transcription = OpenAITranscription()
 
-@app.route("/")
-def home():
-    if 'email' not in session:
-        return redirect(url_for("google_auth_route"))
-    else:
-        return render_template('index.html')
-
-@app.route("/google_auth")
-def google_auth_route():
-    return google_auth()
-
-@app.route("/google_auth/callback")
-def google_auth_callback_route():
-    return google_auth_callback()
-
-
-@app.route("/transcribe")
 def transcribe():
     st.title("Audio Transcription App")
     audio_link = st.text_input("Enter the link to your audio or video file")
@@ -42,5 +18,4 @@ def transcribe():
         except Exception as e:
             st.write("An error occurred: ", str(e))
 
-if __name__ == "__main__":
-    app.run(port=8501, ssl_context='adhoc')
+transcribe()
